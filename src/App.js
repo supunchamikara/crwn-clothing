d9b8859bb2b4e3e2d1c23e1b079b4a5f44108734
supunchamikara <supun.chamikara@hotmailcom>
2020-03-19 14:14:02 +0530

    added redux, and implimented userReducer and userAction to header component.

--- src/App.js
+++ src/App.js
@@ -6,38 +6,29 @@ import SignInAndSignUpPage from './components/sign-in-and-signup/sign-in-and-sig
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.action';
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFormAuth = null;
-
   componentDidMount() {
+    const { setCurrentUser } = this.props
     this.unsubscribeFormAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          }, () => {
-            
-            //console.log(this.state);
           });
         });
-        
+
         //console.log(this.state);
       }
       // createUserProfileDocument(user);
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -48,7 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route exact={true} path='/shop' component={ShopPage} />
@@ -60,4 +51,8 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
