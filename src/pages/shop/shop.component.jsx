2cb119e2eae367b9eeb18e2192e79b5d2c9e2b3d
supunchamikara <supun.chamikara@hotmailcom>
2020-03-22 15:24:55 +0530

    upto data normalization 12-1 video

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,14 @@
-import React from 'react';
-import SHOP_DATA from './shop.data';
-import '../../components/preview-collection/preview-collection.component';
-import CollectionPrewiew from '../../components/preview-collection/preview-collection.component';
+import React from "react";
+import "../../components/preview-collection/preview-collection.component";
+import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
+import { Route } from "react-router-dom";
+import collectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPrewiew key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={collectionPage} />
+  </div>
+);
 
 export default ShopPage;
